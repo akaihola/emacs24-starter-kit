98cc2dd1a1a220f65e394596aa03a859d38af586
Antti Kaihola <akaihol+github@ambitone.com>
2011-08-24 13:07:57 +0300

    Modified init.el for emacs 23 snapshot compatibility on Ubuntu 11.04

--- init.el
+++ init.el
@@ -5,6 +5,23 @@
 ;; This is the first thing to get loaded.
 ;;
 
+(require 'python)
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/package.el"))
+     ;; (expand-file-name "~/.emacs.d/elpa/package.el"))
+     ;; 2011-07-25 akaihola copied package.el from emacs-starter-kit
+  (package-initialize))
+
+
+(require 'org)
+
 ;; remember this directory
 (setq starter-kit-dir
       (file-name-directory (or load-file-name (buffer-file-name))))
@@ -13,3 +30,15 @@
 (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
 
 ;;; init.el ends here
+
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
